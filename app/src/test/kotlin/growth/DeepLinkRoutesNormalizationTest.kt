e1453c25e28cf437fcc8f194356873c8987f5bde
koteev-m <117291255+koteev-m@users.noreply.github.com>
2026-02-03 12:55:44 +0300

    Improve CTA click normalization

---
+++ app/src/test/kotlin/growth/DeepLinkRoutesNormalizationTest.kt
@@ -0,0 +1,30 @@
+package growth
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class DeepLinkRoutesNormalizationTest : FunSpec({
+    test("normalizeAbVariant handles common inputs") {
+        normalizeAbVariant("") shouldBe "NA"
+        normalizeAbVariant("A2") shouldBe "A"
+        normalizeAbVariant("a") shouldBe "A"
+        normalizeAbVariant("B1") shouldBe "B"
+        normalizeAbVariant("control") shouldBe "NA"
+    }
+
+    test("normalizeUserAgent trims, drops empty, and caps length") {
+        normalizeUserAgent("   ") shouldBe null
+        normalizeUserAgent("  Mozilla  ") shouldBe "Mozilla"
+
+        val oversized = "a".repeat(USER_AGENT_MAX_LEN + 10)
+        val normalized = normalizeUserAgent(oversized)
+        normalized?.length shouldBe USER_AGENT_MAX_LEN
+    }
+
+    test("parsePostId returns only positive long values") {
+        parsePostId("123") shouldBe 123L
+        parsePostId("0") shouldBe null
+        parsePostId("-1") shouldBe null
+        parsePostId("abc") shouldBe null
+    }
+})
